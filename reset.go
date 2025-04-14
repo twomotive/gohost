246dca9b79c45ee31b3ebfa8dfe3eb7b309bf824
twomotive <112391727+twomotive@users.noreply.github.com>
2025-04-14 20:25:35 +0300

    feat: add ResetUsers function to reset endpoint for delete  all users from the database.

--- reset.go
+++ reset.go
@@ -1,13 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"os"
 )
 
 func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	cfg.fileServerHits.Store(0)
-	fmt.Fprintf(w, "Hits has been reset to %d", cfg.fileServerHits.Load())
+
+	if os.Getenv("PLATFORM") != "dev" {
+		http.Error(w, "not in development mode!!", http.StatusForbidden)
+		return
+	}
+
+	err := cfg.db.ResetUsers(r.Context())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 
 }
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -7,3 +7,7 @@ VALUES (
     $1
 )
 RETURNING *;
+
+
+-- name: ResetUsers :exec
+DELETE FROM users;
