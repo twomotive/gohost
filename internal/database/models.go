5fa46226abc52020fd3177f906ade655e146bf8b
twomotive <112391727+twomotive@users.noreply.github.com>
2025-04-23 16:37:47 +0300

    feat: add hashed_password field to User model and update related SQL queries

--- internal/database/models.go
+++ internal/database/models.go
@@ -19,8 +19,9 @@ type Gobit struct {
 }
 
 type User struct {
-	ID        uuid.UUID
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Email     string
+	ID             uuid.UUID
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	Email          string
+	HashedPassword string
 }
